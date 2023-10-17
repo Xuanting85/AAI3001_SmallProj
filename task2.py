753f87b0dcd45c1931ac924ccc92b5c80ff0f934
Xuanting85 <xuanting2000@gmail.com>
2023-10-17 23:28:13 +0800

    task2-still fixing

--- task2.py
+++ task2.py
@@ -21,20 +21,13 @@ CLASSES = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industria
 # Number of images to take from each class
 num_images_per_class = 30
 
-# Labelling Changes
-LABEL_CHANGES = {
-    'AnnualCrop': 'PermanentCrop',
-    'PermanentCrop': 'AnnualCrop',
-    'Forest': 'HerbaceousVegetation',
-}
-
 class EuroSATDataset(Dataset):
     def __init__(self, data_dir, transform=None):
         self.data_dir = data_dir
         self.transform = transform
         self.image_paths = []
         self.labels = []
-
+        
         # Load and preprocess the dataset
         self._load_dataset()
 
@@ -42,54 +35,36 @@ class EuroSATDataset(Dataset):
         for i, class_name in enumerate(CLASSES):
             class_path = os.path.join(self.data_dir, class_name)
             for image_name in os.listdir(class_path):
-                self.image_paths.append(os.path.join(class_path, image_name))
+                # Update labels based on the specified multi-labeling changes
+                label = np.zeros(len(CLASSES), dtype=float)
+                label[i] = 1.0  # Set the current class to 1.0
 
-                # Modify the label based on the defined changes
-                modified_label = self._modify_label(class_name)
-                self.labels.append(modified_label)
+                if i == CLASSES.index('AnnualCrop'):
+                    label[CLASSES.index('PermanentCrop')] = 1.0
+                elif i == CLASSES.index('PermanentCrop'):
+                    label[CLASSES.index('AnnualCrop')] = 1.0
+                elif i == CLASSES.index('Forest'):
+                    label[CLASSES.index('HerbaceousVegetation')] = 1.0
+
+                self.labels.append(label.tolist())
+                self.image_paths.append(os.path.join(class_path, image_name))
 
     def __len__(self):
         return len(self.image_paths)
-    
-    def _modify_label(self, original_label):
-        # Modify the label based on the defined changes
-        if original_label in LABEL_CHANGES:
-            original_label = LABEL_CHANGES[original_label]
 
-        # Create a binary label vector
-        modified_label = np.zeros(len(CLASSES), dtype=np.float32)
-        modified_label[CLASSES.index(original_label)] = 1.0
-
-        return modified_label
-    
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
         image = Image.open(image_path)  # Use PIL to open the image
         image = image.convert('RGB')  # Convert to RGB if not already
+        image = np.array(image)  # Convert PIL image to NumPy array
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)  # Convert label to a tensor
 
-        # Ensure 3 channels (RGB)
-        image = np.asarray(image)
-        if image.shape[2] != 3:
-            # Convert grayscale images to RGB by duplicating the channels
-            image = np.stack([image] * 3, axis=-1)
-        
-        # Resize the image to the desired dimensions
-        image = cv2.resize(image, (64, 64))
-
-        # Convert the image to a tensor
-        image = Image.fromarray(image)  # Convert back to PIL Image
         if self.transform:
+            # Apply the transformation to get a tensor
             image = self.transform(image)
-        
-        # Convert the image to a tensor
-        image = transforms.ToTensor()(image)
-
-        # Transpose to match the model's expected input shape
-        image = image.permute(2, 0, 1)
-
-        label = self.labels[idx]
 
         return image, label
+
 
 def load_and_split_dataset(data_dir, random_seed=42):
     # Lists to store data and labels
@@ -106,9 +81,20 @@ def load_and_split_dataset(data_dir, random_seed=42):
             image = cv2.imread(image_path)[:, :, ::-1]  # Read image in BGR, convert to RGB
             image = cv2.resize(image, (64, 64))  # Resize image
             data.append(image)
-            labels.append(i)
+            
+            # Update labels based on the specified multi-labeling changes
+            label = np.zeros(len(CLASSES), dtype=int)
+            label[i] = 1  # Set the current class to 1
 
-    # Convert data and labels to numpy arrays
+            if i == CLASSES.index('AnnualCrop'):
+                label[CLASSES.index('PermanentCrop')] = 1
+            elif i == CLASSES.index('PermanentCrop'):
+                label[CLASSES.index('AnnualCrop')] = 1
+            elif i == CLASSES.index('Forest'):
+                label[CLASSES.index('HerbaceousVegetation')] = 1
+
+            labels.append(label.tolist())
+
     data = np.array(data)
     labels = np.array(labels)
 
@@ -118,18 +104,20 @@ def load_and_split_dataset(data_dir, random_seed=42):
 
     return (train_data, train_labels), (val_data, val_labels), (test_data, test_labels)
 
-def build_model(num_classes, input_channels=3, image_size=64):
+def build_model(num_classes):
+    # Load the pre-trained ResNet-50 model
     model = resnet50(weights=ResNet50_Weights.DEFAULT)
-    # Modify the fully connected layer to match the number of classes
-    num_ftrs = model.fc.in_features
-    model.fc = nn.Sequential(
-        nn.Linear(num_ftrs, num_classes),
-        nn.Sigmoid()  # Use Sigmoid activation for multi-label classification
-    )
     
-    # Define the loss function and optimizer for multi-label classification
-    criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Freeze all layers except the final classification layer
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    # Modify the final classification layer for the specified number of classes
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    
+    # Define the loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
     
     return model, criterion, optimizer
 
@@ -139,9 +127,9 @@ def train_model(model, train_loader, criterion, optimizer, device):
 
     for images, labels in train_loader:
         images = images.permute(0, 3, 1, 2)  # Change the dimension order
-        images = images.float()  # Convert images to FloatTensor
-        labels = labels.float()  # Convert labels to FloatTensor for multi-label
-        images, labels = images.to(device), labels.to(device)  # Convert to appropriate device
+        images = images.float().to(device)  # Convert images to FloatTensor and move to the device
+        images = images[:, :3, :, :]  # Keep only the first 3 channels (for RGB)
+        labels = labels.to(device)  # Convert labels to LongTensor and move to the device
 
         # Zero the parameter gradients
         optimizer.zero_grad()
@@ -162,52 +150,48 @@ def train_model(model, train_loader, criterion, optimizer, device):
 
     return average_loss
 
-def validate_model(model, val_loader, criterion, device):
-    model.eval()  # Set the model to evaluation mode
+def validate_model(model, val_loader, criterion, device, transform):
+    model.eval()
     running_loss = 0.0
     all_preds = []
     all_labels = []
 
     with torch.no_grad():
         for images, labels in val_loader:
-            images = images.float()
-            images = images.permute(0, 3, 1, 2)
-            labels = labels.float()
-            
-            images, labels = images.to(device), labels.to(device)  # Convert to the appropriate device
+            transformed_images = []  # List to store transformed images
+            for img in images:
+                # Assuming the shape of the image is compatible with (64, 64)
+                img_reshaped = img.reshape((64, 64))  # Reshape the image to (64, 64)
+                img_pil = Image.fromarray(np.uint8(img_reshaped), mode='L')  # Convert NumPy array to PIL image (grayscale)
+                transformed_img = transform(img_pil)  # Apply the transformation
+                transformed_images.append(transformed_img)
 
-            # Forward pass
+            # Stack the transformed images to create a batch
+            images = torch.stack(transformed_images)
+            images = images.float().to(device)
+            labels = labels.float().to(device)
+
             outputs = model(images)
             loss = criterion(outputs, labels)
             running_loss += loss.item()
 
-            # Collect predictions and true labels for metrics
-            all_preds.extend(outputs.cpu().numpy())
+            _, preds = torch.max(outputs, 1)
+            all_preds.extend(preds.cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
 
-    # Calculate average loss for the epoch
     average_loss = running_loss / len(val_loader)
 
-    # Convert predictions to binary labels (multi-label thresholding)
-    binary_preds = np.array(all_preds) > 0.5
+    one_hot_labels = label_binarize(all_labels, classes=list(range(len(CLASSES))))
+    one_hot_preds = label_binarize(all_preds, classes=list(range(len(CLASSES))))
 
-    # Convert all_labels and binary_preds to numpy arrays for correct indexing
-    all_labels_np = np.array(all_labels)
-    binary_preds_np = np.array(binary_preds)
-
-    # Calculate average precision for each class
     avg_precision_per_class = []
-    for class_idx in range(len(CLASSES)):
-        avg_precision = average_precision_score(all_labels_np[:, class_idx], binary_preds_np[:, class_idx])
-        avg_precision_per_class.append(avg_precision)
-
-    # Compute accuracy per class
     accuracy_per_class = []
     for class_idx in range(len(CLASSES)):
-        accuracy_class = accuracy_score(all_labels_np[:, class_idx], binary_preds_np[:, class_idx])
+        avg_precision = average_precision_score(one_hot_labels[:, class_idx], one_hot_preds[:, class_idx])
+        accuracy_class = accuracy_score(one_hot_labels[:, class_idx], one_hot_preds[:, class_idx])
+        avg_precision_per_class.append(avg_precision)
         accuracy_per_class.append(accuracy_class)
 
-    # Compute the average accuracy over all classes
     avg_accuracy = sum(accuracy_per_class) / len(CLASSES)
 
     return average_loss, avg_precision_per_class, accuracy_per_class, avg_accuracy
@@ -220,10 +204,10 @@ def test_model(model, test_loader, criterion, device):
 
     with torch.no_grad():
         for images, labels in test_loader:
-            images = images.float()
+            images = images.float().to(device)
             images = images.permute(0, 3, 1, 2)
             images = images[:, :3, :, :]
-            images, labels = images.to(device), labels.long().to(device)
+            images, labels = images.to(device), labels.float().to(device)  # Convert labels to float
 
             # Forward pass
             outputs = model(images)
@@ -261,24 +245,18 @@ def test_model(model, test_loader, criterion, device):
 
 
 def main():
-    print("EuroSAT Dataset Classification")
+    print("EuroSAT Dataset Multi-Label Classification")
     print("=" * 50)
 
     transform = transforms.Compose([transforms.ToTensor()])
     dataset = EuroSATDataset(DATA_DIR, transform=transform)
-    # print(f"Total number of samples in the dataset: {len(dataset)}")
 
     (train_data, train_labels), (val_data, val_labels), (test_data, test_labels) = load_and_split_dataset(DATA_DIR)
-    # Convert labels to binary format
-    train_labels = label_binarize(train_labels, classes=list(range(len(CLASSES))))
-    val_labels = label_binarize(val_labels, classes=list(range(len(CLASSES))))
-    test_labels = label_binarize(test_labels, classes=list(range(len(CLASSES))))
-
     print(f"Number of samples in training set: {len(train_data)}")
     print(f"Number of samples in validation set: {len(val_data)}")
     print(f"Number of samples in test set: {len(test_data)}")
 
-    model, criterion, optimizer = build_model(len(CLASSES), input_channels=3, image_size=64)
+    model, criterion, optimizer = build_model(len(CLASSES))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
@@ -293,11 +271,11 @@ def main():
     test_labels = np.array(test_labels)
 
     # Create DataLoader for training set
-    train_dataset = TensorDataset(torch.tensor(train_data), torch.tensor(train_labels))
+    train_dataset = EuroSATDataset(DATA_DIR, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
     # Create DataLoader for validation set
-    val_dataset = TensorDataset(torch.tensor(val_data), torch.tensor(val_labels))
+    val_dataset = EuroSATDataset(DATA_DIR, transform=transform)
     val_loader = DataLoader(val_dataset, batch_size=32)
 
     # Create DataLoader for test set
@@ -326,7 +304,7 @@ def main():
         train_losses.append(train_loss)
 
         # Validation Loop with data augmentation
-        val_loss, val_avg_precision, val_accuracy, avg_accuracy = validate_model(model, val_loader, criterion, device)
+        val_loss, val_avg_precision, val_accuracy, avg_accuracy = validate_model(model, val_loader, criterion, device, transform)
         print(f"Validation Loss: {val_loss:.4f}")
         print(f"Validation Average Precision per Class: {val_avg_precision}")
         print(f"Validation Accuracy per Class: {val_accuracy}")
@@ -339,7 +317,7 @@ def main():
                 batch_size=32
             )
             augmented_val_loss, aug_val_avg_precision, aug_val_accuracy, avg_aug_val_accuracy = validate_model(model, augmented_val_loader, criterion, device)
-            
+
             print(f"\nValidation with {augment_name}")
             print(f"Validation Loss: {augmented_val_loss:.4f}")
             print(f"Validation Average Precision per Class: {aug_val_avg_precision}")
@@ -352,7 +330,7 @@ def main():
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             best_model = model
-    
+
     print("=" * 50)
 
     # Test the best model on the test set
@@ -366,7 +344,7 @@ def main():
     plt.figure(figsize=(10, 5))
     plt.plot(range(1, num_epochs+1), train_losses, label='Training Loss')
     plt.plot(range(1, num_epochs+1), val_losses, label='Validation Loss')
-    plt.xticks(range(1, num_epochs+1)) 
+    plt.xticks(range(1, num_epochs+1))
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Training and Validation Loss')
